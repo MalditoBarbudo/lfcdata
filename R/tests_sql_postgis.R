d3108d81c41f096f089c47e564437ae04646c63d
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-02-13 18:22:46 +0100

    i have a way semi-working, compared with stars crop method results in the same data. need to research more to know how to get the cropped raster in postgis and use it with stars

--- R/tests_sql_postgis.R
+++ R/tests_sql_postgis.R
@@ -1,10 +1,21 @@
+library(lfcdata)
+lidardb <- lidar()
+
+supposedly_good_results <- sf::read_sf(
+  lidardb$.__enclos_env__$private$pool_conn, 'lidar_municipios'
+) %>%
+  dplyr::select(poly_id, mean_dbh)
+
 wkt_polys <-
-  sf::read_sf('~/Descargas/user_polygons.gpkg') %>%
-  sf::st_transform(crs = 3043) %>%
+  sf::read_sf(lidardb$.__enclos_env__$private$pool_conn, 'lidar_municipios') %>%
+  dplyr::select(poly_id, geometry) %>%
+  dplyr::slice(1:5) %>%
   sf::st_geometry() %>%
   sf::st_as_text(EWKT = TRUE)
 
-names_polys <- c("Portbou", "Lles de Cerdanya", "Sant Feliu de Codines", "Vilaplana", "Puigpelat")
+names_polys <- c(
+  "Abella de la Conca", "Abrera", "Àger", "Agramunt", "Aguilar de Segarra"
+)
 
 vars <- c("public.dbh")
 
@@ -13,43 +24,60 @@ feat_query <- glue::glue_sql(
   .con = lidardb$.__enclos_env__$private$pool_conn
 )
 
-foo_query <- glue::glue_sql(
-  "SELECT poly_id, geometry, ST_SummaryStats(ST_Clip(rast,1,geometry, true)) As stats
+sf::st_read(
+  lidardb$.__enclos_env__$private$pool_conn, query = feat_query, as_tibble = TRUE
+)
+
+b_stats_query <- glue::glue_sql(
+  "SELECT poly_id, geometry, (ST_SummaryStats(ST_Clip(rast,1,geometry, true))).*
          FROM dbh
        INNER JOIN ({feat_query}) AS feat
        ON ST_Intersects(feat.geometry,rast)",
   .con = lidardb$.__enclos_env__$private$pool_conn
 )
 
-b_stats_query <- glue::glue_sql(
-  "SELECT poly_id, geometry, (stats).* FROM ({foo_query}) AS foo",
-  .con = lidardb$.__enclos_env__$private$pool_conn
+sf::st_read(
+  lidardb$.__enclos_env__$private$pool_conn, query = b_stats_query, as_tibble = TRUE
 )
+
+# b_stats_query <- glue::glue_sql(
+#   "SELECT poly_id, geometry, (stats).* FROM ({foo_query}) AS foo",
+#   .con = lidardb$.__enclos_env__$private$pool_conn
+# )
+#
+# sf::st_read(
+#   lidardb$.__enclos_env__$private$pool_conn, query = b_stats_query, as_tibble = TRUE
+# ) %>%
+#   dplyr::group_by(poly_id) %>% dplyr::mutate(sum(sum)/sum(count))
 
 lidar_query <- glue::glue_sql(
-  "SELECT poly_id, geometry, SUM(mean*count)/SUM(count) As mean_dbh
+  "SELECT poly_id, geometry, SUM(sum)/SUM(count) As mean_dbh
     FROM ({b_stats_query}) AS b_stats
-     WHERE count > 0
+   WHERE count > 0
    GROUP BY poly_id, geometry
    ORDER BY poly_id;",
   .con = lidardb$.__enclos_env__$private$pool_conn
 )
 
-lidar_query <- glue::glue(
-  "WITH
-     feat AS (SELECT '{names_polys}' As poly_id, ST_GeomFromEWKT('{wkt_polys}') As geometry),
-     b_stats AS (SELECT poly_id, geometry, (stats).* FROM (
-       SELECT poly_id, geometry, ST_SummaryStats(ST_Clip(rast,1,geometry, true)) As stats
-         FROM {tolower(vars)}
-       INNER JOIN feat
-       ON ST_Intersects(feat.geometry,rast)
-     ) As foo)
-   SELECT poly_id, geometry, SUM(mean*count)/SUM(count) As mean_{vars} FROM b_stats
-     WHERE count > 0
-   GROUP BY poly_id, geometry
-   ORDER BY poly_id;",
-  .con = lidardb$.__enclos_env__$private$pool_conn
+sf::st_read(
+  lidardb$.__enclos_env__$private$pool_conn, query = lidar_query, as_tibble = TRUE
 )
+
+# lidar_query <- glue::glue(
+#   "WITH
+#      feat AS (SELECT '{names_polys}' As poly_id, ST_GeomFromEWKT('{wkt_polys}') As geometry),
+#      b_stats AS (SELECT poly_id, geometry, (stats).* FROM (
+#        SELECT poly_id, geometry, ST_SummaryStats(ST_Clip(rast,1,geometry, true)) As stats
+#          FROM {tolower(vars)}
+#        INNER JOIN feat
+#        ON ST_Intersects(feat.geometry,rast)
+#      ) As foo)
+#    SELECT poly_id, geometry, SUM(mean*count)/SUM(count) As mean_{vars} FROM b_stats
+#      WHERE count > 0
+#    GROUP BY poly_id, geometry
+#    ORDER BY poly_id;",
+#   .con = lidardb$.__enclos_env__$private$pool_conn
+# )
 
 # get and join the results
 res <- lidar_query %>%
@@ -61,3 +89,48 @@ res <- lidar_query %>%
   sf::st_as_sf()
 
 # lidardb$.__enclos_env__$private$pool_conn
+
+
+lidardb$get_data('DBH', 'stars') %>%
+  sf::st_crop(supposedly_good_results[['geometry']][1]) %>%
+  plot(reset = FALSE)
+plot(supposedly_good_results[['geometry']][1], add = TRUE, col = NA, border = 'green')
+
+foo <- stars::read_stars(
+  '../03_lidar_app/lidarappkg/data-raw/DBH.tif', proxy = TRUE
+)
+
+sf::st_crop(foo, supposedly_good_results[['geometry']][1]) %>%
+  stars::st_as_stars() %>%
+  sf::st_crop(supposedly_good_results[['geometry']][1]) %>%
+  purrr::map(~ structure(.x, dim = NULL)) %>%
+  tibble::as_tibble() %>%
+  dplyr::summarise_all(.funs = mean, na.rm = TRUE)
+
+
+  plot(reset = FALSE)
+plot(supposedly_good_results[['geometry']][1], add = TRUE, col = NA, border = 'green')
+
+  stars::
+  purrr::map(~ structure(.x, dim = NULL)) %>%
+  tibble::as_tibble() %>%
+  dplyr::summarise_all(.funs = mean, na.rm = TRUE)
+
+seq_along(supposedly_good_results[['geometry']][1:5]) %>%
+  purrr::map_dfr(
+    .f = ~ sf::st_crop(foo, supposedly_good_results[['geometry']][.x]) %>%
+      purrr::map(~ structure(.x, dim = NULL)) %>%
+      tibble::as_tibble() %>%
+      dplyr::summarise_all(.funs = mean, na.rm = TRUE)
+  )
+
+sf::st_read(
+  lidardb$.__enclos_env__$private$pool_conn,
+  as_tibble = TRUE,
+  query = glue::glue_sql(
+    "SELECT ST_Clip(rast,1,ST_GeomFromEWKT({wkt_polys[1]}), true) FROM dbh
+       INNER JOIN ({feat_query}) AS feat
+       ON ST_Intersects(feat.geometry,rast);",
+    .con = lidardb$.__enclos_env__$private$pool_conn
+  )
+)
